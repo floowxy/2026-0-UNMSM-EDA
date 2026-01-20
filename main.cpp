1e0af1e6ea10a86fead932edfe085c0005e20306
floowxy <alejsot1234@gmail.com>
2026-01-20 07:03:47 -0500

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "util.h"
 #include "pointers.h"
+#include "containers/array.h"
 
 // g++ -std=c++17 -Wall -g -pthread main.cpp -o main
 
 int main() {
     std::cout << "Hello EDA-UNMSM!" << std::endl;
     // DemoUtil();
-    DemoPointers1();
+    // DemoPointers1();
+    DemoArray();
     return 0;
 }
