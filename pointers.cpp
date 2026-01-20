dcf01e2aa26beb5d6808cb637834781ab732b832
floowxy <alejsot1234@gmail.com>
2026-01-20 07:06:42 -0500

    update

--- pointers.cpp
+++ pointers.cpp
@@ -13,32 +13,45 @@ void f3(int *pi){   ++*pi;   pi = nullptr;  }
 void f4(int *&rpi){   ++*rpi;   rpi = nullptr;  }
 
 void DemoPointers1(){
-    int a = 10, &c = a; // b=20,
-    int *p = nullptr; //, *q = nullptr, *r;
-    // r = nullptr;
+    int x = 10, &ri = x; // b=20,
+    int *p1 = nullptr;  //, *q = nullptr, *ri;
+    int **pp = nullptr;
 
-    print("Before f1, a = ", a);
-    f1(a);
+    p1 = &x;
+    pp = &p1;
+
+    print("Before f1, x = ", x);
+    f1(x);
     f1(5);
-    f1(a+7);
-    f1(c);
-    print("After  f1, a = ", a);
+    f1(x+7);
+    f1(ri);
+    print("After  f1, x = ", x);
     
-    a = 10;
-    print("Before f2, a = ", a);
-    f2(a);
+    x = 10;
+    print("Before f2, x = ", x);
+    f2(x);
     // f2(35);
-    // f2(a+4);
-    f2(c);
-    print("After  f2, a = ", a);
+    // f2(x+4);
+    f2(*p1);
+    f2(**pp);
+    print("After  f2, x = ", x);
 
 
-    a = 10; p = &a;
+    x = 10; p1 = &x;
     // b = 20; //q = &b;
-    print("Before f3, a = ", a);
-    f3(&a);
-    f3(p);
-    print("After  f3, a = ", a);
+    print("Before f3, x = ", x);
+    f3(&x);
+    f3(&ri);
+    f3(p1);
+    f3(*pp);
+    print("After  f3, x = ", x);
 
-
+    x = 10; p1 = &x;
+    // b = 20; //q = &b;
+    print("Before f4, x = ", x);
+    // f4(&x);
+    f4(*pp); // p1 = nullptr
+    p1 = &x; // reset p1
+    f4(p1);
+    print("After  f4, x = ", x);
 }
